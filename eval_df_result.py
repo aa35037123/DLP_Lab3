8f14fc0293849ad372ba4949d1915a0a55b8fea7
aa35037123 <wesleyshi0321@gmail.com>
2023-08-08 17:07:57 +0800

    updat evalute

--- eval_df_result.py
+++ eval_df_result.py
@@ -12,15 +12,16 @@ from dataloader import LeukemiaLoader
 from dataloader import getData
 from torch.utils.data import Dataset, DataLoader
 
-def find_highest_acc(path_df):
+def find_highest_acc(path_df, model_name):
     df = pd.read_csv(path_df) if os.path.exists(path_df) else None
     highest_acc = 0
-    for acc in df['resnet18 test']:
+    test_col_name = model_name +' test'
+    for acc in df[test_col_name]:
         if acc > highest_acc:
             highest_acc = acc
     return highest_acc
 root = os.getcwd()
-epochs_lr = '50-0005'
+epochs_lr = '30-001'
 _path_df_ResNet18 = os.path.join(root, 'csv_file', 'df_ResNet18 '+epochs_lr+'.csv')
 _path_df_ResNet50 = os.path.join(root, 'csv_file', 'df_ResNet50 '+epochs_lr+'.csv')
 _path_df_ResNet152 = os.path.join(root, 'csv_file', 'df_ResNet152 '+epochs_lr+'.csv')
@@ -38,5 +39,11 @@ _path_test_result_ResNet18 = os.path.join(root, 'csv_file/110611008_result_resne
 _path_test_result_ResNet50 = os.path.join(root, 'csv_file/110611008_result_resnet50 '+epochs_lr+'.csv')
 _path_test_result_ResNet152 = os.path.join(root, 'csv_file/110611008_result_resnet152 '+epochs_lr+'.csv')
 if __name__ == '__main__':
-    highest_acc = find_highest_acc(_path_df_ResNet18)
-    print(f'resnet18 train\'s best acc : {highest_acc}')
+    model_name = input('Please input model name you want to evaluate : ')
+    if model_name == 'resnet18':
+        highest_acc = find_highest_acc(_path_df_ResNet18, model_name)
+    elif model_name == 'resnet50':
+        highest_acc = find_highest_acc(_path_df_ResNet50, model_name)
+    else:
+        highest_acc = find_highest_acc(_path_df_ResNet152, model_name)
+    print(f'{model_name} test\'s best acc : {highest_acc:.2f}')
